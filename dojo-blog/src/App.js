6ced2ced943280e4d6b1a9d3ebfee627103b629d
Shaun <shaun@thenetninja.co.uk>
2020-12-12 14:16:34 +0000

    lesson-5

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  // const person = { name: 'yoshi', age: 30 };
+  const link = 'http://www.google.com';
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+
+        {/* <p>{ person }</p> */}
+
+        <p>{ 10 }</p>
+        <p>{ "hello, ninjas" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
