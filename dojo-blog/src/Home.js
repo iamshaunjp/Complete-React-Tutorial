2748ef77f709d35e977b28eb48f6d6cdf9343b7d
Shaun <shaun@thenetninja.co.uk>
2020-12-13 16:38:12 +0000

    lesson-8

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,18 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+  // let name = 'mario';
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
 
-  const handleClick = (e) => {
-    console.log('hello ninjas', e);
-  }
-
-  const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e.target);
+  const handleClick = () => {
+    // name = 'luigi';
+    setName('luigi');
+    setAge(30);
   }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{ name } is { age } years old</p>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
     </div>
   );
 }
