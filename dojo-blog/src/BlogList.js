44bc2167bcd5b2204b51e3bfc053d5fe53951801
Shaun <shaun@thenetninja.co.uk>
2020-12-13 17:00:14 +0000

    lesson-12

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,8 +1,4 @@
 const BlogList = ({ blogs, title}) => {
-  // const blogs = props.blogs;
-  // const title = props.title;
-  // console.log(blogs);
-
   return (
     <div className="blog-list">
       <h2>{ title }</h2>
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs" />
     </div>
   );
 }
