d2e210f6aa4c6f2e1a52066adf24cb1470f9250c
Shaun <shaun@thenetninja.co.uk>
2020-12-13 16:26:12 +0000

    lesson-6

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
       <h1>The Dojo Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <a href="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</a>
       </div>
     </nav>
   );
