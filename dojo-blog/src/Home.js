ca4acd546b6a6276f02a48b1c4fcb5b3b50087be
Shaun <shaun@thenetninja.co.uk>
2020-12-13 17:17:56 +0000

    lesson-14

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -12,6 +12,11 @@ const Home = () => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
+
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+  })
 
   return (
     <div className="home">
