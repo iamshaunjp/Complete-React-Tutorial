557cbb0d9b3c565b0eba613f672a875dfe9e19f2
Shaun <shaun@thenetninja.co.uk>
2020-12-13 17:03:59 +0000

    lesson-13

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogs, title}) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
   return (
     <div className="blog-list">
       <h2>{ title }</h2>
@@ -6,6 +6,7 @@ const BlogList = ({ blogs, title}) => {
         <div className="blog-preview" key={blog.id} >
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
+          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -8,10 +8,14 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
-      <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs" />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
 }
