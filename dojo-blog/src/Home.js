f5e647895ac8a75273bc9a48a2d0127b998055f0
Shaun <shaun@thenetninja.co.uk>
2020-12-13 16:33:21 +0000

    lesson-7

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log('hello ninjas', e);
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log('hello ' + name, e.target);
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
     </div>
   );
 }
