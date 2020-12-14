b056b3074495ff7769df72a8490e141c6d8c0d45
Shaun <shaun@thenetninja.co.uk>
2020-12-14 09:02:15 +0000

    lesson-19

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,24 +4,35 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
       .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
         return res.json();
       })
       .then(data => {
         setIsPending(false);
         setBlogs(data);
+        setError(null);
+      })
+      .catch(err => {
+        // auto catches network / connection error
+        setIsPending(false);
+        setError(err.message);
       })
     }, 1000);
   }, [])
 
   return (
     <div className="home">
+      { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
-      {blogs && <BlogList blogs={blogs} />}
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
   );
 }
