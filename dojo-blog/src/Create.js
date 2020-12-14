6eed8527be9746c78cff9041485bb5e3500d2342
Shaun <shaun@thenetninja.co.uk>
2020-12-14 13:52:27 +0000

    lesson-28

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -5,10 +5,17 @@ const Create = () => {
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    console.log(blog);
+  }
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input 
           type="text" 
