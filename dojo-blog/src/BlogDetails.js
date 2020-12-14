a976efbceafac6695781caadc70171f4bf2b97d1
Shaun <shaun@thenetninja.co.uk>
2020-12-14 13:46:14 +0000

    lesson-26

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
 
   return (
     <div className="blog-details">
-      <h2>Blog details - { id }</h2>
+      { isPending && <div>Loading...</div> }
+      { error && <div>{ error }</div> }
+      { blog && (
+        <article>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+          <div>{ blog.body }</div>
+        </article>
+      )}
     </div>
   );
 }
