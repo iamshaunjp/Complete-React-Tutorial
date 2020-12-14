269f566a438416e0d4906221aadd74fca0466f23
Shaun <shaun@thenetninja.co.uk>
2020-12-14 11:59:53 +0000

    lesson-24

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
       .then(res => {
         if (!res.ok) { // error coming back from server
           throw Error('could not fetch the data for that resource');
@@ -20,11 +22,18 @@ const useFetch = (url) => {
         setError(null);
       })
       .catch(err => {
-        // auto catches network / connection error
-        setIsPending(false);
-        setError(err.message);
+        if (err.name == 'AbortError') {
+          console.log('fetch aborted')
+        } else {
+          // auto catches network / connection error
+          setIsPending(false);
+          setError(err.message);
+        }
       })
     }, 1000);
+
+    // abort the fetch
+    return () => abortCont.abort();
   }, [url])
 
   return { data, isPending, error };
