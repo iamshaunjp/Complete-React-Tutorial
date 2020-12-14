ee625bd31068b5bc4b884b2d03e283421c8a7603
Shaun <shaun@thenetninja.co.uk>
2020-12-14 14:04:00 +0000

    lesson-32

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
