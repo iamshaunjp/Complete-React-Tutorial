a8f5f061ebd74f63d01bbd467d47897c0382ad31
Shaun <shaun@thenetninja.co.uk>
2020-12-14 13:55:24 +0000

    lesson-29

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -9,7 +9,13 @@ const Create = () => {
     e.preventDefault();
     const blog = { title, body, author };
 
-    console.log(blog);
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+    })
   }
 
   return (
