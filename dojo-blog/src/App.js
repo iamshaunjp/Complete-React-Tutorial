48d90aa562758796bad049f537b4ac517645aba0
Shaun <shaun@thenetninja.co.uk>
2020-12-14 11:14:32 +0000

    lesson-22

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   return (
@@ -9,8 +10,11 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
             </Route>
           </Switch>
         </div>
---
+++ dojo-blog/src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+    </div>
+  );
+}
+ 
+export default Create;
