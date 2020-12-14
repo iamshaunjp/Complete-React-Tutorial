a2e7b361fd41f49bc5c1811ac29f87cf4cd25aa5
Shaun <shaun@thenetninja.co.uk>
2020-12-14 11:55:14 +0000

    lesson-23

--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>The Dojo Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create" style={{ 
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{ 
           color: 'white', 
           backgroundColor: '#f1356d',
           borderRadius: '8px' 
-        }}>New Blog</a>
+        }}>New Blog</Link>
       </div>
     </nav>
   );
