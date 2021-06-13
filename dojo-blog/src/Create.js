7472bd5f76e8f0e0ca789f2af6e0dd9f33e17d54
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-06-13 14:24:48 +0530

    Update Create.js

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -4,10 +4,12 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
+    setIsPending(true);
 
     fetch('http://localhost:8000/blogs/', {
       method: 'POST',
@@ -15,6 +17,7 @@ const Create = () => {
       body: JSON.stringify(blog)
     }).then(() => {
       console.log('new blog added');
+      setIsPending(false)
     })
   }
 
@@ -43,7 +46,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
       </form>
     </div>
   );
